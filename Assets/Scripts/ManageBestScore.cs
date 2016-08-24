b02c995dec711816804f228558874932a1f46244
galhajaj <galhajaj@gmail.com>
2016-08-24 23:28:48 +0300

    best score and finishing

--- Assets/Scripts/ManageBestScore.cs
+++ Assets/Scripts/ManageBestScore.cs
@@ -5,10 +5,14 @@ using System.Collections;
 public class ManageBestScore : MonoBehaviour 
 {
     public Text BestScoreText;
+    public Text TeaseText;
 	// Use this for initialization
 	void Start () 
     {
         BestScoreText.text = "Best Score: " + PlayerPrefs.GetFloat("BestScore").ToString("F2");
+
+        if (PlayerPrefs.GetFloat("BestScore") == 10.0F)
+            TeaseText.text = "OMG! A Perfect 10.00!!! Cheater...";
 	}
 	
 	// Update is called once per frame
