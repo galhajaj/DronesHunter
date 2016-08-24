2197acb0289a083bed3da819cbbd81ee1b2efd79
galhajaj <galhajaj@gmail.com>
2016-08-24 21:35:53 +0300

    slider time

---
+++ Assets/Scripts/HandleTimeSlider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HandleTimeSlider : MonoBehaviour 
+{
+    public Slider TimeSlider;
+    // ================================================================================== //
+	void Start () 
+    {
+        TimeSlider.value = 10.0F + PlayerPrefs.GetFloat("TimeBonus");
+	}
+    // ================================================================================== //
+	void Update () 
+    {
+        TimeSlider.value -= Time.deltaTime;
+
+        if (TimeSlider.value <= 0.0F)
+            SceneManager.LoadScene("mainScene");
+	}
+    // ================================================================================== //
+}
