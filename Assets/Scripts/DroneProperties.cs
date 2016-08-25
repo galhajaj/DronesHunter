bf794c4a548aaad97c2bf80945553edc6e1e008a
galhajaj <galhajaj@gmail.com>
2016-08-26 00:58:44 +0300

    fix bug - no smoke in big drones

--- Assets/Scripts/DroneProperties.cs
+++ Assets/Scripts/DroneProperties.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class DroneProperties : MonoBehaviour 
 {
-
+    public Sprite BigDroneSprite;
 	// Use this for initialization
 	void Start () 
     {
         if (PlayerPrefs.GetString("LevelType") == "Easy")
-            this.transform.localScale = new Vector3(2.0F, 2.0F);
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = BigDroneSprite;
+            Destroy(this.gameObject.GetComponent<BoxCollider2D>());
+            this.gameObject.AddComponent<BoxCollider2D>();
+            //this.transform.localScale = new Vector3(2.0F, 2.0F);
+        }
 	}
 	
 	// Update is called once per frame
